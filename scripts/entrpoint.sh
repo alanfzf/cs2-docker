7031718e3ad3e987ed691cf5d59d806a171f9e7b
alanfzf <alandavidg13@gmail.com>
2024-03-31 16:40:46 -0600

    :adhesive_bandage: (hotfix): steamcmd fixes

--- scripts/entrpoint.sh
+++ scripts/entrpoint.sh
@@ -4,15 +4,20 @@ echo "Starting CS2 server installation"
 
 mkdir -p "${STEAM_CS_DIR}" || true
 
-# fix for cs
+# steamclient.so fix
+eval bash "${STEAM_CMD_DIR}/steamcmd.sh" +quit
+
+mkdir -p "${STEAM_HOME}/.steam/sdk64"
+mkdir -p "${STEAM_HOME}/.steam/sdk32"
+
+ln -sfT ${STEAM_CMD_DIR}/linux64/steamclient.so "${STEAM_HOME}/.steam/sdk64/steamclient.so"
+ln -sfT ${STEAM_CMD_DIR}/linux32/steamclient.so "${STEAM_HOME}/.steam/sdk32/steamclient.so"
+
+# download cs
 eval bash "${STEAM_CMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_CS_DIR}" \
   +login anonymous \
   +app_update 730 validate \
   +quit
-
-# steamclient.so fix
-mkdir -p "${STEAM_HOME}/.steam/sdk64"
-ln -sfT ${STEAM_CMD_DIR}/linux64/steamclient.so "${STEAM_HOME}/.steam/sdk64/steamclient.so"
 
 echo "Starting server with token: ${STEAM_TOKEN}"
 
