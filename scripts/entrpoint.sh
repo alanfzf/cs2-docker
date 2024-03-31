f7c70c9cd695dc354da54e0bd07c8542f64480da
alanfzf <alandavidg13@gmail.com>
2024-03-31 17:55:53 -0600

    :wrench: (tweak): Removed validate from script

--- scripts/entrpoint.sh
+++ scripts/entrpoint.sh
@@ -2,22 +2,20 @@
 
 echo "Starting CS2 server installation"
 
+# Create base directories
 mkdir -p "${STEAM_CS_DIR}" || true
-
-# steamclient.so fix
-eval bash "${STEAM_CMD_DIR}/steamcmd.sh" +quit
-
 mkdir -p "${STEAM_HOME}/.steam/sdk64"
 mkdir -p "${STEAM_HOME}/.steam/sdk32"
-
-ln -sfT ${STEAM_CMD_DIR}/linux64/steamclient.so "${STEAM_HOME}/.steam/sdk64/steamclient.so"
-ln -sfT ${STEAM_CMD_DIR}/linux32/steamclient.so "${STEAM_HOME}/.steam/sdk32/steamclient.so"
 
 # download cs
 eval bash "${STEAM_CMD_DIR}/steamcmd.sh" +force_install_dir "${STEAM_CS_DIR}" \
   +login anonymous \
-  +app_update 730 validate \
+  +app_update 730 \
   +quit
+
+# Fix for steamclient.so
+ln -sfT ${STEAM_CMD_DIR}/linux64/steamclient.so "${STEAM_HOME}/.steam/sdk64/steamclient.so"
+ln -sfT ${STEAM_CMD_DIR}/linux32/steamclient.so "${STEAM_HOME}/.steam/sdk32/steamclient.so"
 
 echo "Starting server with token: ${STEAM_TOKEN}"
 
